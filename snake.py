5d1250b51a2365634c8181d155f133a4d1d598ed
jun9hwn <junghwan0228@gmail.com>
2025-02-18 14:39:07 +0900

    정환

--- snake.py
+++ snake.py
@@ -1,7 +1,7 @@
 # %% [markdown]
 # ### Pygame Example
 
-# %% [markdown]
+# %% [markdown] #윤정환
 # #### 0. 예제 설명(Introduction)
 # Pygame은 쉽게 Game을 제작할 수 있도록 만들어진 module의 집합입니다.
 # Python과 제공되는 간단한 몇가지의 함수만을 사용하여 실제로 구동할 수 있는 수준으로 만들 수 있습니다.
